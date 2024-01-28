09c5840695388da32c784235b5f7c22156d00a2c
saqar-khodadadi <khodadadi_1342@yahoo.com>
2024-01-28 17:00:50 +0330

    -------

--- Persistence/Migrations/20240128132636_PasswordName.Designer.cs
+++ Persistence/Migrations/20240128132636_PasswordName.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persistence.Data;
 
@@ -11,9 +12,10 @@ using Persistence.Data;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(AnnouncementContext))]
-    partial class AnnouncementContextModelSnapshot : ModelSnapshot
+    [Migration("20240128132636_PasswordName")]
+    partial class PasswordName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Persistence/Migrations/20240128132636_PasswordName.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class PasswordName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Password_PasswordSalt",
+                schema: "SSO",
+                table: "User",
+                newName: "PasswordSalt");
+
+            migrationBuilder.RenameColumn(
+                name: "Password_PasswordHash",
+                schema: "SSO",
+                table: "User",
+                newName: "PasswordHash");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PasswordSalt",
+                schema: "SSO",
+                table: "User",
+                newName: "Password_PasswordSalt");
+
+            migrationBuilder.RenameColumn(
+                name: "PasswordHash",
+                schema: "SSO",
+                table: "User",
+                newName: "Password_PasswordHash");
+        }
+    }
+}
